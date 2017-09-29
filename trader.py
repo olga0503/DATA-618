7b6dd12628efcd86e1f22a7ead0ec2245e18cc9b
olga0503 <ofomicheva86@gmail.com>
2017-09-29 07:45:49 -0400

    Add files via upload
    updated code

--- trader.py
+++ trader.py
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#title           :menu.py
+#title           :trader.py
 #description     :This program displays an interactive menu on CLI
 #author          :
 #date            :
@@ -16,29 +16,36 @@ import sys, os
 
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
+from datetime import datetime
 
 # Main definition - constants
-menu_actions  = {} 
+
+menu_actions  = {}  
+stoks_titles=("AAPL","AMZN","MSFT","INTC","SNAP")
 
 
+pl={
+     stoks_titles[0]: ['0','0','0','0','0'],
+     stoks_titles[1]: ['0','0','0','0','0'],
+     stoks_titles[2]: ['0','0','0','0','0'],
+     stoks_titles[3]: ['0','0','0','0','0'],
+     stoks_titles[4]: ['0','0','0','0','0'],
+     'Cash': [10000000,10000000],
+ }
 
+blotter=[['b','aapl',0,0,0]]
+stock=[]
 
-
-
-
-# =======================
-#     MENUS FUNCTIONS
-# =======================
 
 # Main menu
 def main_menu():
     os.system('clear')
     
-    print ("Welcome")
+    print ("Welcome,\n")
     print ("Please choose the menu you want to start:")
-    print ("1. Menu 1")
-    print ("2. Trade")
+    print ("1. Trade")
+    print ("2. Show Blotter")
+    print ("3. Show P/L")
     print ("\n0. Quit")
     choice = input(" >>  ")
     exec_menu(choice)
@@ -59,104 +66,239 @@ def exec_menu(choice):
             menu_actions['main_menu']()
     return
 
+def get_price(stock_name):
+      
+     #Get Yahoo quote
+     page = urlopen("https://finance.yahoo.com/quote/" + stock_name)
 
-def scrap(stock_name):
-    
-    
-    page = urlopen("https://finance.yahoo.com/quote/"+stock_name)
-
-    soup = BeautifulSoup(page, "html.parser")
-    price_box = soup.find("span", attrs={"Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"})
-    price = price_box.text
-    
-    return price
+     soup = BeautifulSoup(page, "html.parser")
+     price_box = soup.find("span", attrs={"C(black) Fz(24px) Fw(b)"})
+     price = price_box.text
+       
 
 def print_stocks():
+    print(stocks)
+    print ("{:<8} {:<15} {:<10}".format('Index','Stock','Market Price'))
+    for k, v in stocks.items():
+         label, num = v
+         print ("{:<10} {:<15} {:<10}".format(k, label, num)) 
+         
 
-    for j in stocks:
-         print(j,stocks[j])
+def enter_stock_index():
+     try:
+         stock_name=int(input("Select a stock. Enter a number 1-5: "))
+         if stock_name<0 or stock_name>5:
+             print("Your number is out of range. Enter number a number from 1 to 5!")
+             enter_stock_index()
+     except ValueError:
+         print("That's not an integer!")
+         #apply recursive call
+         enter_stock_index()
+     return stock_name
 
-    return
+def enter_sell_buy_option():
+    
+     sb_option=input("Would you like to sell or to buy. Enter S or B?: ")
+     
+     if sb_option.lower()=="s" or sb_option.lower()=="b":
+         return sb_option
+     else:
+         enter_sell_buy_option()
+
+def enter_number_shares():
+     try:
+         share_num=int(input("How many shares?: "))      
+     except ValueError:
+         print("That's not an integer!")
+         #apply recursive call
+         enter_number_shares()
+     return share_num
+
+def exit_or_back():
+     
+     try:
+         share_num=int(input("Enter 9 to go back or 0 to quit: "))
+         if  share_num ==9:
+             back()
+         elif share_num ==0:
+             exit()
+         else:
+             exit_or_back()
+     except ValueError:
+         print("That's not an integer!")
+         #apply recursive call
+         exit_or_back()
+     return 
 
 def trade():
-     classd={}
-     mainl=[]
-     listt=[]
-     score=int(input("Enter a number: "))
-     name=input("What is your name?: ")
-     listt.append(name) 
-     listt.append(score)   
-     name2=input("What is your name?: ")
-     listt.append(name2)
-     #classd[name]=listt
-     mainl.append(listt)
-     
-     #for i in classd:
-         #print (i,classd[i])    
-     print(mainl)    
 
-     choice = input(" >>  ")
+
+     
+     transactions = []
+     
+     print ("Here is the table of stocks with their current market prices\n")
+     print_stocks()
+     print("")
+     print("Your portfolio size is "+ str(portfolio_size))
+
+     #validate user entries
+     stock_index = enter_stock_index()
+     sb_option = enter_sell_buy_option()
+     
+     if sb_option=='s' and len(blotter) == 0:
+         print("You don't have anything to sell!")
+         trade()
+     else:    
+
+         num_shares=enter_number_shares()
+
+         #check if there are enough shares to sell
+         if sb_option=='s' and pl[stoks_titles[stock_index-1]][1] > num_shares:
+             trade()
+         else:
+         #check funds
+             transaction_cost = float(get_price(stoks_titles[stock_index-1]))*num_shares 
+     
+             if portfolio_size < transaction_cost:
+                 print("You don't have enough funds to purchase stocks")
+                 trade()
+             else:   
+
+                 #create list transactions to store transaction info
+                 transactions.append(sb_option)
+                 transactions.append(stoks_titles[stock_index-1]) 
+                 transactions.append(num_shares)   
+
+                 now=datetime.now()
+                 time_date=str(now.month)+'/'+str(now.day)+'/'+str(now.year)+" "+str(now.hour)+" : "+str(now.minute)
+                 transactions.append(time_date)
+                 transaction_cost = float(get_price(stoks_titles[stock_index-1]))*num_shares 
+                 transactions.append(transaction_cost)
+     
+                 if sb_option == "b":
+                     portfolio_size+=transaction_cost
+                 else:           
+                     portfolio_size-=transaction_cost
+     
+            #add transaction to blotter table
+             blotter.append(transactions)  
+      
+     print (blotter)   
+     
+     print("Your portfolio size is "+ str(portfolio_size))
+     
+     choice = input("Would you like to make another trade? Enter Y or N: ")
      ch = choice.lower()
      if ch=="y":
           trade()
      else:
         main_menu()
-
-     return mainl
-
-
-
-           
         
+     return blotter
 
 
-
-  
+def stock_summary(stock_title):
          
-pl = {
+         num_buys=0
+         num_sold=0
+         wap=0
+         cost_buys=0 
+         upl=0
+         rpl=0
+         pl_line=[]
 
-    #'AAPL': summ(),
-}
+         #collect all transactions about the same stock
 
-def print_pl():
+         for i in range(0, len(blotter)):
+             
+             if blotter[i][1] == stock_title:
+                 stock.append(blotter[i])
+                 
+                 if stock[i][0] == "b":
+                
+                     num_buys+=stock[i][2]
+                     cost_buys+=stock[i][4]
+                     print (stock[i][1],num)
+                 
+                 else:
+                     
+                     num_sold+=stock[i][2]
+                     print (stock[i][1],num)
+             
+             num=num_buys-num_sold
+             print (num)
+             
+             #create line for p/l
+             pl_line.append(stock_title)
+             pl_line.append(num)
+             
+             market_price=float(get_price(stock_title))
+             pl_line.append(market_price) 
+             wap=cost_buys/num_buys
+             pl_line.append(wap)
 
-    for i in pl:
-         print(i,pl[i])
+             if  num_buys > 0:
+                 upl= (market_price - wap)/num_buys
+             else:
+                 upl = 0  
 
-    return
+             if  num_sold > 0:    
+                 rpl= (float(get_price(x)) - wap)/num_sold
+             else:
+                 rpl = 0  
+
+             pl_line.append(upl) 
+             pl_line.append(rpl)   
+             print (wap,upl)             
+         print (stock) 
+         print(pl_line)
+         return pl_line
+ 
+    
+
 # Menu 1
 def menu1():
-
-
-     #for i in range(len(mainl)):
-         
+     portfolio_size = 10000000
+     trade()
+     
      #print bb
-         #summ()
-       
-         print_pl()
-         return
-
+     return
 
 
 # Menu 2
 def menu2():
-    
-    print_stocks()
-    print ("Choose a stock")
-    trade()
-    return
+     #if len(blotter) == 0: 
+         #print("No transactions has been made...")
+     #else:
+         print("Ticker  :  Side (Buy or Sell) : Quantity : Executed Price : Date/Time")
+         for item in blotter:
+             print (item[0]," "*(8-len(item[0])),":",
+                    item[1]," "*(20-len(item[1])),":",
+                    item[2]," "*(10-len(item[2])),":",
+                    item[3]," "*(15-len(item[3])),":",
+                    item[4]," "*(10-len(item[4])),":")
+
+         exit_or_back()
+
+# Menu 3
+def menu3():
+     
+     print ("{:<8} {:<15} {:<10} {:<10} {:<15}".format('Ticker','Position','Market','WAP','UPL','RPL'))
+     for k, v in pl.items():
+         label, num = v
+         print ("{:<10} {:<15} {:<10}{:<10} {:<15}".format(k, v))
+         exit_or_back()
+
+     
+     return
 
 # Back to main menu
 def back():
-    #menu_actions['main_menu']()
-    print_pl()
+    menu_actions['main_menu']()
 
 # Exit program
 def exit():
     sys.exit()
-
-
-
 
 # =======================
 #    MENUS DEFINITIONS
@@ -164,30 +306,32 @@ def exit():
 
 # Menu definition
 
-listt=[]
-#mainl=[]
-bb=[]
-
-
 menu_actions = {
-
     'main_menu': main_menu,
     '1': menu1,
     '2': menu2,
-    '9': back,
+    '3': menu3,
     '0': exit,
-
 }
 
 stocks = {
 
-    '1':['AAPL', scrap("AAPL")],
-    #'AMZN': scrap("AMZN"),
-    #'MSFT': scrap("MSFT"),
-    #'INTC': scrap("INTC"),
-    #'SNAP': scrap("SNAP"),
-}
+    '1': [stoks_titles[0],get_price(stoks_titles[0])], 
+    '2': [stoks_titles[1],get_price(stoks_titles[1])],
+    '3': [stoks_titles[2],get_price(stoks_titles[2])],
+    '4': [stoks_titles[3],get_price(stoks_titles[3])],
+    '5': [stoks_titles[4],get_price(stoks_titles[4])],
+ }
 
+
+pl={
+     stoks_titles[0]: stock_summary(stoks_titles[0]),
+     stoks_titles[1]: stock_summary(stoks_titles[1]),
+     stoks_titles[2]: stock_summary(stoks_titles[2]),
+     stoks_titles[3]: stock_summary(stoks_titles[3]),
+     stoks_titles[4]: stock_summary(stoks_titles[4]),
+     'Cash': [portfolio_size,portfolio_size],
+ }
 
 
 # =======================
@@ -198,4 +342,3 @@ stocks = {
 if __name__ == "__main__":
     # Launch main menu
     main_menu()
-    
